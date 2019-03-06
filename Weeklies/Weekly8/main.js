7af7f52f39ba590b9abe077d981fabf4db800cee
Alec Fisher <alecrfis@usc.edu>
2019-03-05 20:56:46 -0800

    weekly8

---
+++ Weeklies/Weekly8/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("#tate").click(function() {
+    $("img").attr("src", "munster.png");
+  });
+
+  $("#dog").click(function() {
+    $("img").attr("src", "serpentine.png");
+});
+
+});
