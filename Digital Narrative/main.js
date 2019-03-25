7649062a8cacbf27b33fe0ff5cac0ab928336bb7
Alec Fisher <alecrfis@usc.edu>
2019-03-24 21:09:26 -0700

    Digital Narrative

---
+++ Digital Narrative/main.js
@@ -0,0 +1,152 @@
+$(document).ready(function() {
+
+  $("#begin").hover(function() {
+    $("#begin").attr("src", "begin.gif");
+    console.log("hover");
+    // $(this).css("color", "green");
+  }, function (){
+
+    $("#begin").attr("src", "begin.png");
+
+  }
+);
+
+$("#tortoisespeech").hover(function() {
+  $("#tortoisespeech").attr("src", "tortoisespeech.gif");
+  console.log("hover");
+  // $(this).css("color", "green");
+}, function (){
+
+  $("#tortoisespeech").attr("src", "tortoisespeech.png");
+
+  }
+);
+
+$("#harespeech").hover(function() {
+  $("#harespeech").attr("src", "harespeech.gif");
+  console.log("hover");
+  // $(this).css("color", "green");
+}, function (){
+
+  $("#harespeech").attr("src", "harespeech.png");
+
+  }
+);
+
+$("#slowspeech").hover(function() {
+  $("#slowspeech").attr("src", "slowspeech.gif");
+  console.log("hover");
+  // $(this).css("color", "green");
+}, function (){
+
+  $("#slowspeech").attr("src", "slowspeech.png");
+
+  }
+);
+
+$("#steadyspeech").hover(function() {
+  $("#steadyspeech").attr("src", "steadyspeech.gif");
+  console.log("hover");
+  // $(this).css("color", "green");
+}, function (){
+
+  $("#steadyspeech").attr("src", "steadyspeech.png");
+
+  }
+);
+
+$("#chess").hover(function() {
+  $("#chess").attr("src", "chess.gif");
+  console.log("hover");
+  // $(this).css("color", "green");
+}, function (){
+
+  $("#chess").attr("src", "chess.png");
+
+  }
+);
+
+$("#scenery").hover(function() {
+  $("#scenery").attr("src", "scenery.gif");
+  console.log("hover");
+  // $(this).css("color", "green");
+}, function (){
+
+  $("#scenery").attr("src", "scenery.png");
+
+  }
+);
+
+$("#won").hover(function() {
+  $("#won").attr("src", "won.gif");
+  console.log("hover");
+  // $(this).css("color", "green");
+}, function (){
+
+  $("#won").attr("src", "won.png");
+
+  }
+);
+
+$("#lost").hover(function() {
+  $("#lost").attr("src", "lost.gif");
+  console.log("hover");
+  // $(this).css("color", "green");
+}, function (){
+
+  $("#lost").attr("src", "lost.png");
+
+  }
+);
+
+$("#waterfall").hover(function() {
+  $("#waterfall").attr("src", "waterfall.gif");
+  console.log("hover");
+  // $(this).css("color", "green");
+}, function (){
+
+  $("#waterfall").attr("src", "waterfall.png");
+
+  }
+);
+
+$("#flowers").hover(function() {
+  $("#flowers").attr("src", "flowers.gif");
+  console.log("hover");
+  // $(this).css("color", "green");
+}, function (){
+
+  $("#flowers").attr("src", "flowers.png");
+
+  }
+);
+
+$("#finish").hover(function() {
+  $("#finish").attr("src", "finish.gif");
+  console.log("hover");
+  // $(this).css("color", "green");
+}, function (){
+
+  $("#finish").attr("src", "finish.png");
+
+}
+);
+
+$(document).ready(function(){
+
+/*! Fades in page on load */
+$('body').css('display', 'none');
+$('body').fadeOut(2000);
+
+});
+
+$(document).ready(function(){
+
+/*! Fades in page on load */
+$('body').css('display', 'none');
+$('body').fadeIn(2500);
+
+});
+
+
+});
