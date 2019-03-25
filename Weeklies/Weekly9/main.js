df62f91c232f900c28bfbbb5fb314dd595b1a9ac
Alec Fisher <alecrfis@usc.edu>
2019-03-24 21:04:18 -0700

    weekly9

---
+++ Weeklies/Weekly9/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+/*! Fades in page on load */
+$('body').css('display', 'none');
+$('body').fadeOut(2000);
+
+});
+
+$(document).ready(function(){
+
+/*! Fades in page on load */
+$('body').css('display', 'none');
+$('body').fadeIn(2500);
+
+});
